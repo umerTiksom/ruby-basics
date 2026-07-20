8f725bef3040c9e3be1c5679af061b818b173395
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-20 18:37:57 +0500

    update the github after completing the task 2

--- day6/README.md
+++ day6/README.md
@@ -1,2 +1,3 @@
 In the day 6 task
 first task is to create the hello world file and push it on git hub
+second task is to implement variable constants string interpolation user input and outputs
---
+++ day6/introduction.rb
@@ -0,0 +1,18 @@
+#variables
+name= "umer"
+age = 24
+salary = 250000
+puts name,age,salary
+
+#constants
+API_KEY = "hukjksd8kjbd3bsud890-1"
+puts "API key is = #{API_KEY}"
+
+# String interpolation #{}
+puts "my name is #{name}"
+
+#user input
+print "Enter your department = "
+department= gets.chomp
+print "\n"
+puts "my name is #{name}","my age is #{age}","my department is #{department}", "my salary is #{salary}"
