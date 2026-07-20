be65e60a494130245cccd2c4bcc39baaaa2d9175
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-20 18:06:26 +0500

    day6 hello world file commit

---
+++ day6/README.md
@@ -0,0 +1,2 @@
+In the day 6 task
+first task is to create the hello world file and push it on git hub
---
+++ day6/hello_world.rb
@@ -0,0 +1,4 @@
+#print hello world with both puts and print
+puts "hello world"
+print "\n"
+print "hello world"
