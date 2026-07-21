bcaeae7dff8a32e5fee69754763597d4cdd0839a
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-21 12:07:37 +0500

    update day6

--- day6/introduction.rb
+++ day6/introduction.rb
@@ -1,17 +1,19 @@
-#variables
+# variables
 name= "umer"
 age = 24
 salary = 250000
-puts name,age,salary
+puts name, age, salary
+
 #constants
+
 API_KEY = "hukjksd8kjbd3bsud890-1"
 puts "API key is = #{API_KEY}"
 
-# String interpolation #{}
+#String interpolation #{}
 puts "my name is #{name}"
 
 #user input
 print "Enter your department = "
 department= gets.chomp
 print "\n"
-puts "my name is #{name}","my age is #{age}","my department is #{department}", "my salary is #{salary}"
+puts "my name is #{name}","my age is #{age}","my department is #{department}","my salary is #{salary}"
