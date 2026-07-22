5ec34be88f4a06f1f23f53208ff706741fcc84ad
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-22 13:43:47 +0500

    update the practice 8 file

--- day8/practice_day8.rb
+++ day8/practice_day8.rb
@@ -1,4 +1,5 @@
 # if else statement example
+
  number = 0
  print "Enter the number = "
  number = gets.chomp.to_i
@@ -20,4 +21,50 @@ when number.even?
   puts "number is even"
 else
   puts "number is odd"
+end
+
+num=0
+while num<5 do
+  puts "this is #{num} iteration"
+  num+=1
+end
+
+
+# time loops
+5.times do
+  puts "hello"
+end
+
+num = 5
+
+for i in 1..num do
+puts "Welcome"
+
+end
+
+
+num = [1,2,3]
+num.each do |n|
+  puts "this is #{n}"
+end
+
+employees ={
+  employee1:{
+    name:"Muhammad Umer",
+    tittle: "Ruby on Rail"
+  },
+   employee2: {
+    name: "Ali",
+    job_title: "Frontend Developer"
+  },
+  employee3: {
+    name: "Sara",
+    job_title: "UI/UX Designer"
+  }
+}
+employees.each do |emp_id,emp_detail|
+  print "\n"
+  puts "employee id is #{emp_id}"
+  puts "employee name is #{emp_detail[:name]}"
+  puts "employee tittle is #{emp_detail[:job_title]}"
 end
