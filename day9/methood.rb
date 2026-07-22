5eaa63d1a05c6e7c8a4ebc1129f7381278c98e27
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-22 15:56:14 +0500

    Add yhhe methood practice file

---
+++ day9/methood.rb
@@ -0,0 +1,8 @@
+def displayName
+  puts 'hello world'
+end
+displayName
+def displayWithPrams(name:, age:)
+  puts "Name is #{name} and the age is #{age}"
+end
+displayWithPrams(name: 'umer', age: 24)
