b06a96c716123e2c5f3879f55a2e3ca5c922ee3b
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-07-27 15:08:34 +0500

    done the person class file

---
+++ day11/person.rb
@@ -0,0 +1,16 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(p_name, p_age)
+    @name = p_name
+    @age = p_age
+  end
+end
+
+person1 = Person.new('umer', 24)
+person2 = Person.new('ali', 20)
+person3 = Person.new('Taha', 23)
+
+puts person1.name
+person2.name = 'ahmed'
+puts person2.name
--- day7/practice_day7.rb
+++ day7/practice_day7.rb
@@ -1,42 +1,42 @@
 # strings
-name = "umer"
+name = 'umer'
 puts name
 puts name.length
 puts name.upcase
-#used for the lowerCase
+# used for the lowerCase
 puts name.downcase
 puts name.reverse
 puts name.object_id
-testing_task = "Hi my name is umer and I am from tiksom"
-puts testing_task.include?("umer")
+testing_task = 'Hi my name is umer and I am from tiksom'
+puts testing_task.include?('umer')
 # replace the old text with the new text
-puts testing_task.gsub("umer", "Umer Iqbal")
+puts testing_task.gsub('umer', 'Umer Iqbal')
 
-#symbols
-person ={
-  name:"Ali",
+# symbols
+person = {
+  name: 'Ali',
   age: 24
 }
-puts person[:name] #use :name to access the symbols
+puts person[:name] # use :name to access the symbols
 
-#arrays
+# arrays
 
-arr1=[1,2,3,4,5]
+arr1 = [1, 2, 3, 4, 5]
 puts arr1[3]
-arr2 = [1,2,"ali",2.34]
+arr2 = [1, 2, 'ali', 2.34]
 puts arr2[2]
 arr1.push(6)
 arr1.each do |num|
-puts num
+  puts num
 end
 
-#hash (data structure)
+# hash (data structure)
 employee = {
-  name: "Ali",
-  salary: 22500
+  name: 'Ali',
+  salary: 22_500
 }
 puts "salary of the #{employee[:name]} is #{employee[:salary]}"
-number =40
+number = 40
 puts number.even?
-testing_task = "Hi my name is umer and I am from tiksom"
+testing_task = 'Hi my name is umer and I am from tiksom'
 puts testing_task.match?(/access/)
