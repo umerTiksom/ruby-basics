ce735c8b40c135f66f827ba80ab85742c63386dc
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-08-06 12:19:47 +0500

    apply the exit button in the task management app

--- week4/task_management/app.rb
+++ week4/task_management/app.rb
@@ -3,8 +3,17 @@ require_relative 'utils/file_manager'
 require_relative 'model/tasks'
 require_relative 'views/menu'
 require 'date'
+require 'colorize'
 require_relative 'services/projects_services'
 @project_service = Project_services.new
 @task_service = Task_services.new
 menu = Menu.new(@project_service, @task_service)
-menu.start
+loop do
+  menu.start
+  puts 'Press 0 to exit the system or press any key to move to main menu '
+  @user_choice = gets.chomp.to_i
+  if @user_choice == 0
+    puts 'Exit System Successfully...'.green
+    exit
+  end
+end
